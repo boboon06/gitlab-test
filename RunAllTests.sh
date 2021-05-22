848b6851510ab260ece87a8df7691b1ffd013d30
Joshua Holland <jholland@theboboon.com>
2021-05-22 15:21:42 +1200

    [Step3] Making a bash script that runs all simulator tests and and returns 1 if a test fails

---
+++ RunAllTests.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+hasTestFailed=false
+for i in tests/*.in;
+do
+	docker run --mount type=bind,src=tests,dst=/etc/tests $CI_REGISTRY_IMAGE/simulator:$CI_COMMIT_REF_SLUG /etc/tests/$i.in
+	testResult = $?
+	echo "Testing $i.in returned exit code $testResult"
+	if (testResult != 0)
+		hasTestFailed = true
+done
+
+if (hasTestFailed)
+	exit 1

f70df0006974aabef2fb8d91b578fba922d73fa2
Joshua Holland <jholland@theboboon.com>
2021-05-22 16:13:24 +1200

    [Step3] Fixing RunAllTests.sh complaining that it could not find testResult or hasTestFailed because I forgot to add the $ to mark them as a variable

--- RunAllTests.sh
+++ RunAllTests.sh
@@ -8,11 +8,11 @@ do
 	docker run --mount type=bind,src=$(pwd)/tests,dst=/etc/tests $CI_REGISTRY_IMAGE/simulator:$CI_COMMIT_REF_SLUG "/etc/tests/$testName.in"
 	testResult=$?
 	echo "Testing $testName.in returned exit code $testResult"
-	if testResult != 0; then
+	if $testResult != 0; then
 		hasTestFailed=true
 	fi
 done
 
-if hasTestFailed; then
+if $hasTestFailed; then
 	exit 1
 	fi

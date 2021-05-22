c1613498d31eaf75fcb3a0d51fa891746d1dfb53
Joshua Holland <jholland@theboboon.com>
2021-05-22 16:05:28 +1200

    [Step3] Fixing RunAllTests.sh complaining that it could not find testResult or hasTestFailed because I added spaces around the equals when setting the variable

--- RunAllTests.sh
+++ RunAllTests.sh
@@ -6,10 +6,10 @@ do
 	testName=$(basename "$i" .in)
 	echo "Testing $testName.in..."
 	docker run --mount type=bind,src=$(pwd)/tests,dst=/etc/tests $CI_REGISTRY_IMAGE/simulator:$CI_COMMIT_REF_SLUG "/etc/tests/$testName.in"
-	testResult = $?
+	testResult=$?
 	echo "Testing $testName.in returned exit code $testResult"
 	if testResult != 0; then
-		hasTestFailed = true
+		hasTestFailed=true
 	fi
 done
 
